7bc4f155514582cb537e8cb6d30a69d5a073542b
Spencer Dent <sd00070@my.westga.edu>
2021-06-08 10:06:51 -0400

    test adding children to the root Node

--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -15,5 +15,41 @@ describe('BST', function () {
     
             assert.equal(testTree.root.data, testBrick)
         })
+
+        it('should add a Node to the left when the size is less than the root', function () {
+            let testRootBrick = new Brick(4, "red")
+            let testLeftBrick = new Brick(1, "red")
+            let testTree = new BST()
+            
+            testTree.insert(testRootBrick)
+            testTree.insert(testLeftBrick)
+            
+            assert.equal(testTree.root.left.data, testLeftBrick)
+        })
+        
+        it('should add a Node to the right when the size is greater than the root', function () {
+            let testRootBrick = new Brick(4, "red")
+            let testRightBrick = new Brick(8, "red")
+            let testTree = new BST()
+            
+            testTree.insert(testRootBrick)
+            testTree.insert(testRightBrick)
+            
+            assert.equal(testTree.root.right.data, testRightBrick)
+        })
+        
+        it('should support adding children to both sides of the tree', function () {
+            let testRootBrick = new Brick(4, "red")
+            let testLeftBrick = new Brick(1, "red")
+            let testRightBrick = new Brick(8, "red")
+            let testTree = new BST()
+            
+            testTree.insert(testRootBrick)
+            testTree.insert(testLeftBrick)
+            testTree.insert(testRightBrick)
+
+            assert.equal(testTree.root.left.data, testLeftBrick)
+            assert.equal(testTree.root.right.data, testRightBrick)
+        })
     })
 })
