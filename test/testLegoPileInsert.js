7720ab87a298b06cc21f9feeacb7beeb7efa83b9
Spencer Dent <sd00070@my.westga.edu>
2021-06-08 11:29:33 -0400

    test inserting Bricks of all colors

--- test/testLegoPileInsert.js
+++ test/testLegoPileInsert.js
@@ -15,5 +15,30 @@ describe('LegoPile', function () {
 
             assert.equal(pile.colors.red.root.data, oneRedBrick)
         })
+
+        it('(one of each color) should allow inserting into all colors', function () {
+            let pile = new LegoPile()
+
+            const redBrick = new Brick(4, "red")
+            const greenBrick = new Brick(64, "green")
+            const blueBrick = new Brick(6, "blue")
+            const yellowBrick = new Brick(8, "yellow")
+            const blackBrick = new Brick(1, "black")
+            const whiteBrick = new Brick(4, "white")
+
+            pile.insert(redBrick)
+            pile.insert(greenBrick)
+            pile.insert(blueBrick)
+            pile.insert(yellowBrick)
+            pile.insert(blackBrick)
+            pile.insert(whiteBrick)
+
+            assert.equal(pile.colors.red.root.data, redBrick)
+            assert.equal(pile.colors.green.root.data, greenBrick)
+            assert.equal(pile.colors.blue.root.data, blueBrick)
+            assert.equal(pile.colors.yellow.root.data, yellowBrick)
+            assert.equal(pile.colors.black.root.data, blackBrick)
+            assert.equal(pile.colors.white.root.data, whiteBrick)
+        })
     })
 })
