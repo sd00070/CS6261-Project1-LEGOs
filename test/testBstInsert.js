7fb36d5a018d39d8a919d5c4dba4a9564f2dd37b
Spencer Dent <sd00070@my.westga.edu>
2021-06-08 10:13:29 -0400

    label & reformat for better clarity

--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -7,43 +7,47 @@ const BST = bst.BST
 
 describe('BST', function () {
     describe('insert', function () {
-        it('should have a single Node at its root after calling on empty tree', function () {
-            let testBrick = new Brick(4, "red")
+        it('(single node) should have a single Node at its root after calling on empty tree', function () {
             let testTree = new BST()
-    
+
+            let testBrick = new Brick(4, "red")
+
             testTree.insert(testBrick)
-    
+
             assert.equal(testTree.root.data, testBrick)
         })
 
-        it('should add a Node to the left when the size is less than the root', function () {
+        it('(root + left) should add a Node to the left when the size is less than the root', function () {
+            let testTree = new BST()
+
             let testRootBrick = new Brick(4, "red")
             let testLeftBrick = new Brick(1, "red")
-            let testTree = new BST()
-            
+
             testTree.insert(testRootBrick)
             testTree.insert(testLeftBrick)
-            
+
             assert.equal(testTree.root.left.data, testLeftBrick)
         })
-        
-        it('should add a Node to the right when the size is greater than the root', function () {
+
+        it('(root + right) should add a Node to the right when the size is greater than the root', function () {
+            let testTree = new BST()
+
             let testRootBrick = new Brick(4, "red")
             let testRightBrick = new Brick(8, "red")
-            let testTree = new BST()
-            
+
             testTree.insert(testRootBrick)
             testTree.insert(testRightBrick)
-            
+
             assert.equal(testTree.root.right.data, testRightBrick)
         })
-        
-        it('should support adding children to both sides of the tree', function () {
+
+        it('(root + (left & right)) should support adding children to both sides of the tree', function () {
+            let testTree = new BST()
+
             let testRootBrick = new Brick(4, "red")
             let testLeftBrick = new Brick(1, "red")
             let testRightBrick = new Brick(8, "red")
-            let testTree = new BST()
-            
+
             testTree.insert(testRootBrick)
             testTree.insert(testLeftBrick)
             testTree.insert(testRightBrick)
